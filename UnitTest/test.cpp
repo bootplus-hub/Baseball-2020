13031c291443292d351f6c8484b9e627b68f9ac7
juheon.song <bootplus@naver.com>
2024-07-03 15:07:18 +0900

    [feature] page 49

--- UnitTest/test.cpp
+++ UnitTest/test.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 class BaseballFixture : public testing::Test {
 public:
-	Baseball game;
+	Baseball game{ "123" };
 	void assertIllegalArgment(string guessNumber) {
 		try {
 			game.guess(string(guessNumber));
@@ -22,4 +22,11 @@ TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase) {
 	assertIllegalArgment("12");
 	assertIllegalArgment("12s");
 	assertIllegalArgment("121");
+}
+
+TEST_F(BaseballFixture, ReturnSolvedResultIfMatchedNumber) {
+	GuessResult result = game.guess("123");
+	EXPECT_TRUE(result.solved);
+	EXPECT_EQ(3, result.strikes);
+	EXPECT_EQ(0, result.balls);
 }
